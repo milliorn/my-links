b6a71e82054cf532e1b272c0c38074f329bdde78
milliorn <scottmilliorn@gmail.com>
2022-11-27 02:24:42 -0800

    copy text

--- js/index.js
+++ js/index.js
@@ -2,8 +2,17 @@ const shareButtons = document.querySelectorAll(".tile-share-button");
 
 // console.log(shareButtons);
 
-function copyText(e) {
+async function copyText(e) {
   e.preventDefault();
+  const link = this.getAttribute("link");
+  // console.log(link);
+
+  try {
+    await navigator.clipboard.writeText(link);
+    alert("Link was copied: " + link);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 // add click events for each share button
