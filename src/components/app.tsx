1ec712075b7114e590177e097d9d574f96c8044b
milliorn <scottmilliorn@gmail.com>
2022-11-28 00:23:41 -0800

    container, h1

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,10 +1,13 @@
 import { h } from "preact";
+import Container from "./container";
 
 import Header from "./header";
 
 const App = () => (
   <div id="app">
     <Header />
+    <Container />
+    <h1>@scottmilliorn</h1>
   </div>
 );
 
---
+++ src/components/container/index.tsx
@@ -0,0 +1,13 @@
+import { h } from "preact";
+
+const Container = () => {
+  return (
+    <div class="container">
+      <div class="image-container">
+        <img src="../../assets/profile.jpg" alt="Scott Milliorn" />
+      </div>
+    </div>
+  );
+};
+
+export default Container;
