bc11af0bd605e108e000456cca696c5d25f7e07c
milliorn <scottmilliorn@gmail.com>
2022-11-28 01:04:21 -0800

    BusinessCard

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -4,6 +4,7 @@ import { Csv } from "./svg/Csv";
 import Header from "./header";
 import { ShareTileButton } from "./svg/ShareTileButton";
 import { Portfolio } from "./svg/Portfolio";
+import { BusinessCard } from "./svg/BusinessCard";
 
 const App = () => (
   <Fragment>
@@ -54,61 +55,7 @@ const App = () => (
       rel="noreferrer"
     >
       <div className="icon">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
-          <rect width="256" height="256" fill="none" />
-          <line
-            x1="152"
-            y1="112"
-            x2="192"
-            y2="112"
-            fill="none"
-            stroke="#fff"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="12"
-          />
-          <line
-            x1="152"
-            y1="144"
-            x2="192"
-            y2="144"
-            fill="none"
-            stroke="#fff"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="12"
-          />
-          <circle
-            cx="92.1"
-            cy="120"
-            r="24"
-            fill="none"
-            stroke="#fff"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="12"
-          />
-          <path
-            d="M61.1,168a32,32,0,0,1,62,0"
-            fill="none"
-            stroke="#fff"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="12"
-          />
-          <rect
-            x="32"
-            y="48"
-            width="192"
-            height="160"
-            rx="8"
-            fill="none"
-            stroke="#fff"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="12"
-          />
-        </svg>
+        <BusinessCard />
       </div>
       <p>Business Card</p>
       <div
