bf264cdf8d3ee3970233178b55e89b42e3b82c38
Scott Milliorn <scottmilliorn@gmail.com>
2022-11-28 16:48:22 -0800

    Preact (#14)
    * preact
    
    * header
    
    * container, h1
    
    * className
    
    * ShareTileButton
    
    * Portfolio
    
    * BusinessCard
    
    * linkedin
    
    * various fixes, changes, components
    
    * more fixes
    
    * gitignore
    
    * linter changes
    
    * removed css lint

---
+++ src/components/app.tsx
@@ -0,0 +1,49 @@
+import { Fragment, h } from "preact";
+import Container from "./container";
+import Header from "./header";
+import ShareTileButton from "./svg/ShareTileButton";
+import { tileData } from "./tileData";
+
+function App() {
+  async function handleClick(e: MouseEvent): Promise<void> {
+    e.preventDefault();
+    const currentTarget = e.currentTarget as HTMLElement;
+    const link = currentTarget.id;
+
+    try {
+      await navigator.clipboard.writeText(link);
+      alert(`Link ${link} copied to clipboard`);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return (
+    <Fragment>
+      <Header handleClick={handleClick} />
+      <Container />
+      <h1>@scottmilliorn</h1>
+      {tileData.map((tile, i) => (
+        <a
+          key={i}
+          className="tile"
+          href={tile.href}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="icon">{tile.svg}</div>
+          <p>{tile.p}</p>
+          <div
+            className="tile-share-button"
+            id={tile.href}
+            onClick={handleClick}
+          >
+            <ShareTileButton />
+          </div>
+        </a>
+      ))}
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/container/index.tsx
@@ -0,0 +1,13 @@
+import { h } from "preact";
+
+function Container() {
+  return (
+    <div className="container">
+      <div className="image-container">
+        <img src="../../assets/profile.jpg" alt="Scott Milliorn" />
+      </div>
+    </div>
+  );
+}
+
+export default Container;
